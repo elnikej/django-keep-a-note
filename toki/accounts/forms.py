3fdea7479a02267457c6e7fb7bb5daa85d3e34a9
jens <information@pustelnik.de>
2019-05-09 00:49:36 +0200

    optimizations

--- toki/accounts/forms.py
+++ toki/accounts/forms.py
@@ -2,8 +2,8 @@ from __future__ import unicode_literals
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
-from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
+from crispy_forms.layout import Layout, Submit, HTML, Field  # , Button, Row, Div
+# from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
 from authtools import forms as authtoolsforms
 from django.contrib.auth import forms as authforms
 from django.urls import reverse
--- toki/profiles/urls.py
+++ toki/profiles/urls.py
@@ -6,6 +6,5 @@ app_name = 'profiles'
 urlpatterns = [
     url(r'^me$', views.ShowProfile.as_view(), name='show_self'),
     url(r'^me/edit$', views.EditProfile.as_view(), name='edit_self'),
-    url(r'^(?P<slug>[\w\-]+)$', views.ShowProfile.as_view(),
-        name='show'),
+    url(r'^(?P<slug>[\w\-]+)$', views.ShowProfile.as_view(), name='show'),
 ]
